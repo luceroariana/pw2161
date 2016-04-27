57b16fffc91253d022e5f353ae7235656d8faf01
luceroariana <arianar438@gmail.com>
2016-04-27 10:58:05 -0600

    Primer ejemplo JQuery

---
+++ jquery/js/eventos.js
@@ -0,0 +1,17 @@
+// jquery(etiquetas, clases, id)
+// $ alias (un sobrenombre)
+// $ == jquery
+var inicio = function()
+{
+	var clicBoton = function()
+	{
+		console.log("Clic del botón");
+		//$(".anuncioWeb").html("Clic del Botón");
+		$(".anuncioWeb").append("Clic del Botón");
+	}
+	//Preparar los eventos de todos mis objetos
+	$("#miBoton").on("click", clicBoton);
+}
+
+//Main
+$(document).on("ready", inicio); //ENCENDER EL EVENTO
